00b5792efb04c4c95a7e0e1836b2c745abea7fea
oliviadestyriana <oliviadestyriana>
2021-09-19 11:53:48 +0700

    lkp

---
+++ LKP-3/MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/HomeFragment.java
@@ -0,0 +1,46 @@
+package com.example.myflexiblefragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment implements View.OnClickListener  {
+    private Button btnDetailCategory;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btnDetailCategory = (Button) view.findViewById(R.id.btn_category);
+        btnDetailCategory.setOnClickListener(this);
+        return view;
+    }
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_category){
+            CategoryFragment mCategoryFragment = new CategoryFragment();
+            FragmentManager mFragmentManager = getFragmentManager();
+            FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+            mFragmentTransaction.replace(R.id.frame_container, mCategoryFragment, CategoryFragment.class.getSimpleName());
+            mFragmentTransaction.addToBackStack(null);
+            mFragmentTransaction.commit();
+
+            }
+        }
+    }
+
